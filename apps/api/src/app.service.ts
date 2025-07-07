a1a5da7dda17adda49b640daac932cb58bdad8fd
LTenshi <lpawlak192@gmail.com>
2025-07-07 23:19:09 +0100

    API handles no text now

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -7,6 +7,9 @@ export class AppService {
   }
 
   postExampleText(text: string): string {
+    if (!text) {
+      return `API didn't get any text :(`
+    }
     const randomNumber = Math.floor(Math.random() * 5);
     const adjectives = ["Beautiful", "Charming", "Despicable", "Angry", "Mellow"]
     return `This is an example post return: ${adjectives[randomNumber]}-${text}`
